a5e6ada55432be636a7a9fb457848c0915f9bec5
alan <alan@debian>
2020-03-03 14:50:26 -0300

    fixies

--- src/satproc/build_dataset.py
+++ src/satproc/build_dataset.py
@@ -33,7 +33,7 @@ coco_output = {
 }
 
 
-def mask_from_polygons(polygons, win, mask_path, image_index, mask_index):
+def mask_from_polygons(polygons, win, mask_path, src, kwargs, image_index, mask_index):
     if polygons:
         mask = rasterize(polygons, (win.height, win.width),
             transform=rasterio.windows.transform(win, src.transform))
@@ -58,7 +58,7 @@ def build_dataset(
     step_size = 128, 
     output_dir=".", 
     instance = True, 
-    coco_output = False):
+    coco = True):
 
     blocks = fiona.open(dataset_path)
     block_shapes = [shape(b['geometry']) for b in blocks]
@@ -73,7 +73,7 @@ def build_dataset(
         win_step_size = (step_size, step_size)
         windows = list(sliding_windows(win_size, win_step_size, src.shape[1], src.shape[0]))
 
-        for win in windows:
+        for (win, (i, j)) in windows:
             # Write tif tile
             kwargs = src.meta.copy()
             kwargs.update({
@@ -86,7 +86,7 @@ def build_dataset(
                 dst.write(src.read(window=win))
 
             # Append tile info on COCO dataset
-            if coco_output:
+            if coco:
                 image_info = pycococreatortools.create_image_info(
                     k, os.path.basename(dst_name), (win.height, win.width))
                 coco_output["images"].append(image_info)
@@ -102,28 +102,30 @@ def build_dataset(
                 if instance:
                     # For each polygon, create a mask
                     for i, poly in enumerate(intersect_polys):
-                        mask = mask_from_polygons(
-                            [poly], win, os.path.join(output_dir,output_tiles_gt), k, i)
+                        mask = mask_from_polygons([poly], win, os.path.join(output_dir,output_tiles_gt), 
+                            src, kwargs, k, i)
 
-                        if mask and coco_output:
+                        if mask is not None and coco:
                             #Append annotation info on COCO dataset
                             annotation_info = pycococreatortools.create_annotation_info(a, k, CATEGORIES[0], mask, (512,512), tolerance=2)
                             a = a + 1
                             coco_output["annotations"].append(annotation_info)
                 else:
-                    mask = mask_from_polygons(intersect_polys, win, os.path.join(output_dir,output_tiles_gt), k, 0)
+                    mask = mask_from_polygons(intersect_polys, win, os.path.join(output_dir,output_tiles_gt), 
+                        src, kwargs, k, 0)
 
-                    if mask and coco_output:    
+                    if mask is not None and coco:    
                         #Append annotation info on COCO dataset
                         annotation_info = pycococreatortools.create_annotation_info(a, k, CATEGORIES[0], mask, (512,512), tolerance=2)
                         a = a + 1
                         coco_output["annotations"].append(annotation_info)
             else:
                 #If there aren't any polygon, an empty mask is saved
-                mask = mask_from_polygons(None, win, os.path.join(output_dir,output_tiles_gt), k, i)
+                mask = mask_from_polygons(None, win, os.path.join(output_dir,output_tiles_gt), 
+                    src, kwargs, k, i)
 
             k = k + 1
     
-    if coco_output:
+    if coco:
         with open(os.path.join(output_dir,'annotations_coco.json'), 'w') as outfile:
             json.dump(coco_output, outfile)
