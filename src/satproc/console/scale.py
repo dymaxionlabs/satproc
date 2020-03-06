b9bb90a2353af70addb1bd9f23d77af781e929ee
Damián Silvani <munshkr@gmail.com>
2020-03-06 11:45:54 -0300

    New script: scale

---
+++ src/satproc/console/scale.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+[options.entry_points] section in setup.cfg:
+
+    console_scripts =
+         fibonacci = satproc.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+
+import argparse
+import sys
+import logging
+
+from satproc import __version__
+from satproc.scale import scale
+
+__author__ = "Damián Silvani"
+__copyright__ = "Damián Silvani"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Min-max scale a raster",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--version",
+                        action="version",
+                        version="satproc_scale {ver}".format(ver=__version__))
+
+    parser.add_argument("input", help="input raster file")
+    parser.add_argument("output", help="output raster file")
+
+    parser.add_argument("--window-size",
+                        help="window size",
+                        type=int,
+                        default=1024)
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel,
+                        stream=sys.stdout,
+                        format=logformat,
+                        datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+
+    scale(args.input, args.output, window_size=args.window_size)
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/satproc/scale.py
@@ -0,0 +1,59 @@
+import logging
+
+import numpy as np
+import rasterio
+from tqdm import tqdm
+
+from satproc.utils import sliding_windows
+
+__author__ = "Damián Silvani"
+__copyright__ = "Damián Silvani"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def get_min_max(img, window_size=512):
+    mins, maxs = [], []
+    with rasterio.open(img) as src:
+        win_size = (window_size, window_size)
+        windows = list(
+            sliding_windows(win_size, win_size, src.width, src.height))
+        for _, (window, (i, j)) in tqdm(list(enumerate(windows))):
+            img = src.read(window=window)
+            mins.append(
+                np.array([np.nanmin(img[i, :, :]) for i in range(src.count)]))
+            maxs.append(
+                np.array([np.nanmax(img[i, :, :]) for i in range(src.count)]))
+    min_value = np.nanmin(np.array(mins), axis=0)
+    max_value = np.nanmax(np.array(maxs), axis=0)
+    return min_value, max_value
+
+
+def minmax_scale(img, *, min_values, max_values):
+    """Scale bands of +img+ separately, to range 0-1"""
+    n_bands = img.shape[0]
+    return np.array([
+        (img[i, :, :] - min_values[i]) / (max_values[i] - min_values[i])
+        for i in range(n_bands)
+    ])
+
+
+def scale(input_img, output_img, window_size=512):
+    """Scale all bands of +input_img+ separately and save to +output_img+"""
+    min_values, max_values = get_min_max(input_img, window_size=window_size)
+
+    with rasterio.open(input_img) as src:
+        win_size = (window_size, window_size)
+        windows = list(
+            sliding_windows(win_size, win_size, src.width, src.height))
+        profile = src.profile.copy()
+        profile.update(dtype=np.float32)
+        with rasterio.open(output_img, 'w', **profile) as dst:
+            for _, (window, (i, j)) in tqdm(list(enumerate(windows))):
+                _logger.debug("%s %s", window, (i, j))
+                img = src.read(window=window)
+                new_img = minmax_scale(img,
+                                       min_values=min_values,
+                                       max_values=max_values)
+                dst.write(new_img, window=window)
