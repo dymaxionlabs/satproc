dfee09946430bdb12302d7e82b8ff2aefc55b803
Damián Silvani <munshkr@gmail.com>
2021-05-10 11:25:50 -0300

    Fix __version__

--- src/satproc/__init__.py
+++ src/satproc/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
+    dist_name = "pysatproc"
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = "unknown"
