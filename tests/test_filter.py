ac020e2bcde2877fb3c322962fe9b6a2c3a98eaf
Damián Silvani <munshkr@gmail.com>
2022-02-24 12:25:51 -0300

    test_filter

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,11 +1,45 @@
+import tempfile
+from pathlib import Path
+
 import pytest
 
 from satproc import __version__
 from satproc.console.filter import main
+from satproc.filter import filter_by_max_prob
 
 __author__ = "Damián Silvani"
 __copyright__ = "Dymaxion Labs"
 __license__ = "Apache-2.0"
+
+
+def test_filter_valid_threshold(datadir):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        input_dir = datadir / "chips"
+
+        filter_by_max_prob(
+            input_dir=input_dir,
+            output_dir=str(tmpdir),
+            threshold=0.05,
+        )
+
+        output_images_dir = Path(tmpdir)
+        assert output_images_dir.is_dir()
+        assert len(list(output_images_dir.glob("*.tif"))) == 4
+
+
+def test_filter_threshold_too_high(datadir):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        input_dir = datadir / "chips"
+
+        filter_by_max_prob(
+            input_dir=input_dir,
+            output_dir=str(tmpdir),
+            threshold=0.5,
+        )
+
+        output_images_dir = Path(tmpdir)
+        assert output_images_dir.is_dir()
+        assert len(list(output_images_dir.glob("*.tif"))) == 0
 
 
 def test_cli_main(capsys):
