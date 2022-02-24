65d53a55f32d4710fdc65047302ea787b2599d85
Damián Silvani <munshkr@gmail.com>
2022-02-24 13:11:55 -0300

    test_smooth_stitch

--- tests/test_smooth_stitch.py
+++ tests/test_smooth_stitch.py
@@ -1,11 +1,28 @@
+import tempfile
+from pathlib import Path
+
 import pytest
 
 from satproc import __version__
 from satproc.console.smooth_stitch import main
+from satproc.postprocess.smooth import smooth_stitch
 
 __author__ = "Damián Silvani"
 __copyright__ = "Dymaxion Labs"
 __license__ = "Apache-2.0"
+
+
+def test_smooth(datadir):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        input_dir = datadir / "distance"
+        smooth_stitch(
+            input_dir=input_dir,
+            output_dir=tmpdir,
+        )
+
+        output_dir = Path(tmpdir)
+        assert output_dir.is_dir()
+        assert len(list(output_dir.glob("*.tif"))) == 8
 
 
 def test_cli_main(capsys):
